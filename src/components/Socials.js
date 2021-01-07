81634a951fe4e07ee09f0f2a2d1b063aa3d78fda
Adam-Foot <adamfoot16@gmail.com>
2021-01-07 16:57:20 +0000

    Started Socials content

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 const Socials = () => (
-    <div className="container">
+    <div className="container font-big">
         <div className="row">
-            <div className="col-">
-                <p>Socials</p>
+            <div className="col-12">
+                <h1 className="display-4">Social Media.</h1>
+            </div>
+        </div>
+
+        <div className="row mb-5">
+            <div className="col-12 col-md-8 col-lg-7">
+                <p className="text-muted">Below you will be able to find all my useful Social Media links. Connect with me!</p>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-12 col-md-8 col-lg-7">
+                <p className="text-muted">Below you can find all my useful Social Media links. Connect with me!</p>
+            </div>
+        </div>
+
+        <div className="row fixed-bottom">
+            <div className="col-12 text-center">
+                <p className="text-muted font-small">&#169; Copyright 2021 - Adam Foot</p>
             </div>
         </div>
     </div>
