719dcdd2421f172efd50b8080cfdec1c98901ab7
Adam-Foot <adamfoot16@gmail.com>
2021-01-18 15:22:20 +0000

    Added projects page and nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,16 +9,19 @@ const NavBar = () => (
         <button className="navbar-toggler" type="button" data-toggle="collapse"
                 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                 aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon" />
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                    <Link to={ROUTES.CV} className="nav-link font-weight-bold">CV</Link>
+                    <Link to={ROUTES.CV} className="nav-link font-weight-bold text-uppercase">CV</Link>
                 </li>
                 <li className="nav-item">
-                    <Link to={ROUTES.SOCIAL} className="nav-link font-weight-bold">Social Media</Link>
+                    <Link to={ROUTES.PROJECTS} className="nav-link font-weight-bold text-uppercase">Projects</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to={ROUTES.SOCIAL} className="nav-link font-weight-bold text-uppercase">Social Media</Link>
                 </li>
             </ul>
         </div>
