328c0669de77f7780dd7e585d40ec8e8b8c8b34a
Adam-Foot <adamfoot16@gmail.com>
2021-01-18 16:19:37 +0000

    Added projects page and nav links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,32 @@
 import React from "react";
+import twitter from "../assets/twitter.png";
+import github from "../assets/github.png";
+import linkedin from "../assets/linkedin.png";
 
 const Projects = () => {
     return(
-      <></>
+        <div>
+            <div className="container font-big">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="display-4">Projects.</h1>
+                    </div>
+                </div>
+
+                <div className="row mb-5">
+                    <div className="col-12 col-md-8 col-lg-7">
+                        <p className="text-muted">Below you will be able to find all my current and completed
+                        web projects!</p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <p className="text-muted font-small">&#169; Copyright 2021 - Adam Foot</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
